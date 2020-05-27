bfab22ad720f375d5763b80cd2051ec288b0d04c
Pierre Lavalette <lavalette.pierre@gmail.com>
2020-05-27 16:31:19 +0200

    Client Step 1.12: Success adding last message to ChatsList

--- src/component/ChatsListScreen/ChatsList.tsx
+++ src/component/ChatsListScreen/ChatsList.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {chats} from '../../db';
+import moment from "moment";
 
 const ChatsList: React.FC = () => (
         <div>
@@ -9,6 +10,12 @@ const ChatsList: React.FC = () => (
                             <li key={chat.id}>
                                 <img src={chat.picture} alt="Profile"/>
                                 <div>{chat.name}</div>
+                                {chat.lastMessage && (
+                                    <>
+                                        <div>{chat.lastMessage.content}</div>
+                                        <div>{moment(chat.lastMessage.createdAt).format('HH:mm')}</div>
+                                    </>
+                                    )}
 
                             </li>
                         );
