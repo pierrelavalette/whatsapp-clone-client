3fcb9613562d6f3d91e2cd3f1294725e53418430
Pierre Lavalette <lavalette.pierre@gmail.com>
2020-05-27 15:10:47 +0200

    Client Step 1.6: Move ChatsNavbar to a component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ChatsList from "./component/ChatsList";
+import ChatsNavbar from "./component/ChatsNavbar";
 
 const App: React.FC = () => (
   <div>
-    <div>Whatsapp Clone</div>
+    <ChatsNavbar />
     <ChatsList />
   </div>
 );
---
+++ src/component/ChatsNavbar.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const ChatsNavbar: React.FC = () => (
+    <div>
+        Whatsapp Clone
+    </div>
+);
+
+export default ChatsNavbar;
+
